b8227bdf99ee5347b1b9be4fab609e44085387a9
aguowei <317170754@qq.com>
2017-03-27 20:48:13 +0800

    '修改了一个路径'

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/','Front\IndexController@indexa' );
 
 Route::get('/index','Front\IndexController@index' );
 
-Route::get('/','IndexController@indexa' );
+
 
 
