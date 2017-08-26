b9e37839ed790b280c055b7ba05eaca78b199c47
zzpzixuan <1125592459@qq.com>
2017-08-26 17:33:53 +0800

    tian jia le duanxin tongzhi

--- app/Console/Commands/SendPhone.php
+++ app/Console/Commands/SendPhone.php
@@ -37,6 +37,70 @@ class SendPhone extends Command
      */
     public function handle()
     {
-        //
+        $this->mypost();
     }
+
+    function mypost(){
+
+        $url = 'http://disclosure.szse.cn/m/search0425.jsp';
+        $data = array(
+            'leftid' => 1,
+            'lmid' => 'drgg',
+            'pageNo' => 1,
+            'stockCode' => '000018',
+            'keyword' => '',
+            'noticeType' => '',
+            'startTime' => '2017-08-24',
+            'endTime' => '2017-08-26',
+            'imageField.x' => 40,
+            'imageField.y' => 14,
+            'tzy' => ''
+        );
+
+        $ch = curl_init();
+        curl_setopt($ch,CURLOPT_URL,$url);
+        curl_setopt($ch,CURLOPT_CUSTOMREQUEST,'POST');
+        if (!empty($data)) {
+            if (is_array($data)) {
+                $data = http_build_query($data);
+            }
+            curl_setopt($ch,CURLOPT_POSTFIELDS,$data);
+            $header['Content-type:'] = 'application/x-www-form-urlencoded';
+        }
+        if (!empty($header)) {
+            curl_setopt($ch,CURLOPT_HTTPHEADER,$header);
+        }
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($ch,CURLOPT_HEADER,false);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        $this->handle_data($result);
+    }
+
+    function handle_data($result){
+        $result = iconv( "GB2312//IGNORE", "UTF-8",$result) ; //print_r($result);die;
+        $pre = '/<td align="left">([\s\S]+)<\/tbody><\/table><\/td><\/tr>/';
+        preg_match($pre,$result,$res);
+
+        $rel_arr = explode('</tr>',$res[1]);
+        array_pop($rel_arr);
+
+        foreach($rel_arr as $val){
+            $pre_href = "/<a href='([\s\S]+)PDF'/";
+            preg_match($pre_href,$val,$rel_href);
+            $rel_href = $rel_href[1].'PDF';
+
+            $pre_title = '/target="new">([\s\S]+)<\/a>/';
+            preg_match($pre_title,$val,$rel_title);
+
+            //发送短信
+            $title = $rel_title[1];
+            $url = 'http://disclosure.szse.cn/'.$rel_href;
+            \sendMSG::baseInfo($title,$url);
+        }
+    }
+    
+    
+    
+    
 }
---
+++ app/Library/sendMSG.php
@@ -0,0 +1,73 @@
+
+<?php
+class sendMSG{
+    static public function baseInfo($title,$url){
+
+        $apikey = "54cae413bd079b5fc80e601f70add178"; //修改为您的apikey(https://www.yunpian.com)登录官网后获取
+        $mobile = "18600681925"; //请用自己的手机号代替
+        $mobile_array = array("18600681925","13602069161","13904343290","18610713978","13621120036");
+
+        $ch = curl_init();
+        /* 设置验证方式 */
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Accept:text/plain;charset=utf-8',
+            'Content-Type:application/x-www-form-urlencoded',
+            'charset=utf-8')
+        );
+
+        /* 设置返回结果为流 */
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        /* 设置超时时间*/
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        /* 设置通信方式 */
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+        // 取得用户信息
+        $json_data = self::get_user($ch,$apikey);
+        //$array = json_decode($json_data,true);
+
+        // 发送模板短信
+        // 需要对value进行编码
+        foreach($mobile_array as $val){
+            $data=array('tpl_id'=>'1933080',
+                'tpl_value' => '#title#'.'='.urlencode($title),
+                'apikey' => $apikey,
+                'mobile' => $val);
+
+            $json_data = self::tpl_send($ch,$data);
+            $array = json_decode($json_data,true);
+            echo '<pre>';print_r($array);
+        }
+
+       die;
+
+    }
+
+    //获得账户
+    static public function get_user($ch,$apikey){
+        curl_setopt ($ch, CURLOPT_URL, 'https://sms.yunpian.com/v2/user/get.json');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('apikey' => $apikey)));
+        $result = curl_exec($ch);
+        $error = curl_error($ch);
+        self::checkErr($result,$error);
+        return $result;
+    }
+
+    static public function tpl_send($ch,$data){
+        curl_setopt ($ch, CURLOPT_URL, 'https://sms.yunpian.com/v2/sms/tpl_single_send.json');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+        $result = curl_exec($ch);
+        $error = curl_error($ch);
+        self::checkErr($result,$error);
+        return $result;
+    }
+
+    static function checkErr($result,$error) {
+        if($result === false) {
+            echo 'Curl error: ' . $error;die;
+        }else {
+            //echo '操作完成没有任何错误';
+            }
+    }
+}
