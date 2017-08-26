de694d742c5ee6ec9e773917a4f41a7909c127e0
zzpzixuan <1125592459@qq.com>
2017-08-26 12:33:12 +0800

    xiu gai le pei zhi

--- app/Http/Controllers/Front/IndexController.php
+++ app/Http/Controllers/Front/IndexController.php
@@ -2,13 +2,18 @@
 
 namespace App\Http\Controllers\Front;
 
+use App\model\Tt;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class IndexController extends Controller
 {
 
     public function index(){
+
+       // $rel = DB::table('tt')->select('*')->get(); print_r($rel);die;
+        $rel = Tt::find(1); print_r($rel);die;
        return view('front.index');
     }
 
---
+++ app/model/Tt.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tt extends Model
+{
+    //
+    protected $table = 'tt';
+}
