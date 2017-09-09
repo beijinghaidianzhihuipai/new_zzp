744bc3e95d6268759b8a76e358c6e46eb75e0ad9
zzpzixuan <1125592459@qq.com>
2017-09-09 22:58:58 +0800

    xiugai le cuowu

--- app/model/ZzpUser.php
+++ app/model/ZzpUser.php
@@ -3,9 +3,13 @@
 namespace App\model;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Passport\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class ZzpUser extends Model
+class ZzpUser extends Authenticatable
 {
+    use HasApiTokens, Notifiable;
     protected $table = 'user';
     protected $fillable = [
         'name',
