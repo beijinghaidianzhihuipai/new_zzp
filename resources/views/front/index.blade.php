23deb04a6632f9759c5149c2bb079bf17aad5220
zzpzixuan <1125592459@qq.com>
2017-11-10 00:26:42 +0800

    xiu gai ye mian

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -2,7 +2,20 @@
 
 @section('content')
 <div id="neirong">
-<div class="change_img"><img src="/images/front/shouye/FQ01T004T2.jpg"/></div>
+    <div class="change_img"><img src="/images/front/shouye/FQ01T004T2.jpg"/></div>
+    <div class="pro_data">
+        <div class="pro_mid">
+            <div class="pro_img"><img src="/images/front/shouye/ppjj.jpg"/></div>
+            <div class="pro_info">
+                    <p> <span style="color:#f01b2f;"><span style="font-size:20px;"><strong>智者派简介</strong></span></span></p>
+                智者派网始终坚持网站内容的权威性和专业性，打造中国财经航母。<br>
+                网站内容涉及财经、股票、基金、期货、债券、外汇、银行、保险等诸多金融资讯与财经信息，全面覆盖财经领域，每日更新上万条最新数据及资讯，为用户提供便利的查询。<br>
+                网站全体员工以“没有休息日、新闻不过夜”作为工作口号，保证让广大用户在第一时间内尽览天下财经及理财资讯。<br>
+                智者派网从用户需求出发，打造中国最大、最火爆的互动平台，深受用户喜爱。<br>
+
+                </div>
+        </div>
+    </div>
 </div>
 @endsection
 
--- resources/views/front/public/footer.blade.php
+++ resources/views/front/public/footer.blade.php
@@ -1,6 +1,6 @@
 
 <div id="footer">
-    <div class="comp_info">
+    <div id="comp_info">
         <a>Copyright © 2017-2018 北京智者派信息科技网络平台 版权所有    京ICP备14042879号-1</a>
     </div>
 </div>
