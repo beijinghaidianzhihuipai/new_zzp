1edd1ed53b805059a3430ca2dec511e46c255d14
zzpzixuan <1125592459@qq.com>
2017-11-12 15:57:40 +0800

    xinchuangkou dakai

--- resources/views/front/proclamation.blade.php
+++ resources/views/front/proclamation.blade.php
@@ -4,7 +4,7 @@
 <div id="neirong">
 <ul class="wei">
 @foreach($stock_infos as $value)
-    <li class="aa"><a href="{{$value->url}}">{{$value->title}} </a> &nbsp &nbsp &nbsp  {{$value->report_date}}</li>
+    <li class="aa"><a href="{{$value->url}}" target="_blank">{{$value->title}} </a> &nbsp &nbsp &nbsp  {{$value->report_date}}</li>
 @endforeach
 </ul>
 {!! $stock_infos->links() !!}
