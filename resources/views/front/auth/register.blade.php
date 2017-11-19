aa6d59de8a442eadcca3a950084708281d2fcccc
longsonghao <384240007@qq.com>
2017-11-19 17:11:27 +0800

    zaici gengxin

--- resources/views/front/auth/register.blade.php
+++ resources/views/front/auth/register.blade.php
@@ -1,38 +1,6 @@
 ﻿@extends('front/public/app')
 @section('content')
-        <!--
-<form method="POST" action="/front/register">
-    {!! csrf_field() !!}
-    <div>
-        用户名
-        <input type="text" name="name" value="{{ old('name') }}">
-    </div>
 
-    <div>
-        手机号
-        <input type="text" name="phone_num" value="{{ old('phone_num') }}">
-    </div>
-
-    <div>
-        Email
-        <input type="email" name="email" value="{{ old('email') }}">
-    </div>
-
-    <div>
-        密码
-        <input type="password" name="password">
-    </div>
-
-    <div>
-        确认密码
-        <input type="password" name="password_confirmation">
-    </div>
-
-    <div>
-        <button type="submit">提交注册</button>
-    </div>
-</form>
--->
 <div id="main">
     <div class="user-center bdr8">
         <div class="uc-hd p-rel">注册</div>
