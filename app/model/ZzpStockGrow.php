046b2dfeff1e8ace58a158d6dc388263fc4267a0
zzpzixuan <1125592459@qq.com>
2017-12-06 17:39:39 +0800

    xiu gai le  lianxu xiadie

--- app/model/ZzpStockGrow.php
+++ app/model/ZzpStockGrow.php
@@ -31,13 +31,26 @@ class ZzpStockGrow extends Model
     }
 
     public static function getDownData($down_days){
+
+        $last_rel = self::select()->orderby('id','DESC')->first();
+        $more_rel = self::select()
+            ->where('stock_code',$last_rel->stock_code)
+            ->orderby('id','DESC')
+            ->limit($down_days)
+            ->get();
+
+        $ch_num = $down_days-1;
+        $ch_time = $more_rel[$ch_num]->stock_date;
         $where = array(
-            array('stock_time','>',0),
+            array('stock_time','>=',$ch_time),
+            array('grow_type','=',2),
         );
         $rel = self::select('stock_name','stock_code','stock_type','end_price',
             DB::raw('sum(grow_price) as grow_price'),DB::raw('count(id) as num'))
-            ->where($where)->limit(25)->groupBy('stock_code')
-            ->having('num', '>=', $down_days)->orderBy('grow_price', 'asc')->get();
+            ->where($where)->groupBy('stock_code')
+            ->having('num', '>=', $down_days)
+            ->orderBy('grow_price', 'asc')
+            ->limit(25)->get();
 
         return empty($rel) ? '' : $rel->toArray();
     }
