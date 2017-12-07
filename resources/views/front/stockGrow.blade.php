f5c7003d9cf800bea0cd751a00bcd2b979596192
zzpzixuan <1125592459@qq.com>
2017-12-07 22:11:39 +0800

    you hua tu

--- resources/views/front/stockGrow.blade.php
+++ resources/views/front/stockGrow.blade.php
@@ -18,6 +18,75 @@
     </div>
 </div>
 
+
+    <div id="f_main" class="none">
+        <div id="f_title">
+            <div id="f_ten"></div>
+            <div id="f_img" title="close"></div>
+        </div>
+        <div id="f_content">
+        </div>
+    </div>
+
+
+    <style>
+        #f_main{position: absolute;top: 30%;right: 10%;}
+        #f_title{width: 500px;height: 20px;border-top: #85ABE4 1px solid;
+            border-right: #222 1px solid;border-left: #85ABE4 1px solid;border-bottom: none;background: #5B8BD9;}
+        #f_content{border: #85ABE4 1px solid;border-top: none;
+            background:#fff; width: 500px;height: 310px;}
+        #f_content img{width: 500px;height: 310px;}
+        #off{float: right;cursor: pointer;}
+        .none{display: none;}
+        .show{display: block;}
+        #f_ten{float: left;width: 440px;height:20px;}
+        #f_img{background: url({{URL::asset('/images/front/s.jpg')}}) no-repeat;float: right;
+            width: 60px;
+            height: 20px;
+        }
+        #f_img:hover{background: url({{URL::asset('/images/front/sh.jpg')}}) no-repeat;
+            float: right;
+            cursor:pointer;
+            width: 60px;
+            height: 20px;}
+
+    </style>
+
+    <script type="text/javascript">
+        $(document).ready(function(){
+
+            //窗口的关闭
+            $("#f_img").click(function() {
+                $("#f_main").removeClass("show");
+                $("#f_main").addClass("none");
+                 });
+
+             //窗口的拖动
+            var bool=false;
+            var offsetX=0;
+            var offsetY=0;
+            $("#f_ten").mousedown(function(){
+                bool=true;
+                offsetX = event.offsetX;
+                offsetY = event.offsetY;
+                $("#f_ten").css('cursor','move');
+            })
+
+            $("#f_main").mouseup(function(){
+                        bool=false;
+                    })
+            $(document).mousemove(function(e){
+                if(!bool)
+                    return;
+                var x = event.clientX-offsetX;
+                var y = event.clientY-offsetY;
+                $("#f_main").css("left", x);
+                $("#f_main").css("top", y);
+            })
+
+        });
+    </script>
+
 <script>
     //页面加载完后执行
     $(function(){
@@ -42,7 +111,7 @@
                     }else{
                         var code = "'sz" + data[i].stock_code + "'";
                     }
-                    con += '<li class="grow" onclick="drawImg(' +code+ ')"><a>' + data[i].stock_name +
+                    con += '<li id="grow" onclick="drawImg(' +code+ ')"><a>' + data[i].stock_name +
                             "(" + data[i].stock_code + ")" +
                             "&nbsp;&nbsp;&nbsp;下跌：" + data[i].grow_price +"元"+
                             "&nbsp; &nbsp;&nbsp; 价格：" + data[i].end_price +"元"+
@@ -58,9 +127,10 @@
     }
 
     function drawImg(codes){
-
+        $("#f_main").removeClass("none");
+        $("#f_main").addClass("show");
         var img = '<img src="http://image.sinajs.cn/newchart/daily/n/' + codes + '.gif"  />';
-        $(".draw").html('').append(img);
+        $("#f_content").html('').append(img);
     }
 </script>
 @endsection
